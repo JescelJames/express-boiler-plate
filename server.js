95ca46c627018140a6b112613c32717c3b1d6a00
Jescel James <jesceljames@gmail.com>
2023-12-21 09:49:26 -0500

    server live on port 3001

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // DEPENDENCIES
 // express
+const express = require('express');
+
+
 
 // DATA
 
 // APP / PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 // MIDDLEWARES
 
 
+
 // ROUTES
 
 
-
-
 // START SERVER
-
+app.listen(PORT, () => console.log(`Server life on port ${PORT}` ));
